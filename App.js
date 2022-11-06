84e330257ce853d1dea537de85c6c4d3cecfa63b
Lineke09 <109102261+Lineke09@users.noreply.github.com>
2022-11-06 11:50:15 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import HelloWorld from "./components/HelloWorld";
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Matheus Lineke da Cruz Figueredo</h1>
+    <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
